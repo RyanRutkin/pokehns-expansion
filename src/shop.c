4fa81079474ee6e2d26700d5b25bc4e6c99db081
Ryan Rutkin <rutkin.ryan@gmail.com>
2026-09-01 16:26:25 -0500

    Added SOLD OUT text when an item has reached the custom stock amount for the day

--- src/shop.c
+++ src/shop.c
@@ -1401,7 +1401,8 @@ static void BuyMenuPrintPriceInList(u8 windowId, u32 itemId, u8 y)
                 6);
         }
 
-        if (GetItemImportance(itemId) && (CheckBagHasItem(itemId, 1) || CheckPCHasItem(itemId, 1)))
+        if ((GetItemImportance(itemId) && (CheckBagHasItem(itemId, 1) || CheckPCHasItem(itemId, 1)))
+         || GetRemainingShopStock(itemId) == 0)
             StringCopy(gStringVar4, gText_SoldOut);
         else
             StringExpandPlaceholders(gStringVar4, gText_PokedollarVar1);
